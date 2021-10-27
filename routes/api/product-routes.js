a11acf884a87a9b8a874c54062308bdf3ee94d42
David Abbey <davidabbey@earthlink.net>
2021-10-27 16:53:12 -0700

    think everything built, get good hits on insomnia, and populated tables on workbench. deploy to heroku tonight or in the morning and test

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,50 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+      model: Category,
+      attributes: ['category_name']
+    },
+    {
+      model:"Tag",
+      attributes:['tag_name']
+    }]
+  }).then(dbProdData=>res.json(dbProdData))
+  .catch(err=>{
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where:{
+      id: req.params.id
+    },
+    attributes:['id', 'product_name', 'price', 'stock'],
+    include:[{
+      model: Category,
+      attributes:['category_name']
+    },
+    {
+      model:"Tag",
+      attributes:['tag_name']
+    }]
+  }).then(dbProdData=>{
+    if(!dbProdData){
+      res.status(404).json({message:"No product found with this ID."})
+    }else{
+      res.json(dbProdData)
+    }
+  }).catch(err=>{
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 // create new product
@@ -25,8 +63,12 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
-    .then((product) => {
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body,stock,
+    tagIds:req.body.tagIds
+  }).then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
@@ -39,8 +81,7 @@ router.post('/', (req, res) => {
       }
       // if no product tags, just respond
       res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
+    }).then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
@@ -91,6 +132,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where:{
+      id:req.params.id
+    }
+  }).then(dbProdData=>{
+    if(!dbProdData){
+      res.status(404).json({message:"No product found with this ID."})
+    }else{
+      res.json(dbProdData)
+    }
+  }).catch(err=>{
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 module.exports = router;
